0609de0501abbbc1fa1cb3bcf5c75b264743ca3f
DESKTOP-0BEF0BT\PC-24 <bomik0614@gmail.com>
2021-02-17 21:11:20 +0900

    20210217 commit - users테이블 변경에 맞춰 VO 내용 변경 - 회원가입시 입력항목에 대한 정규화 추가(닉네임 보류)

--- playddit/src/users/vo/UsersVO.java
+++ playddit/src/users/vo/UsersVO.java
@@ -2,11 +2,16 @@ package users.vo;
 
 public class UsersVO {
 	private String user_id;
-	private String user_name;
-	private String user_password;
-	private String user_email;
-	private String user_tel;
 	private String user_nickname;
+	private String user_password;
+	private String user_name;
+	private String user_tel;
+	private String user_birth;
+	private String user_class;
+	private int user_rating;
+	private int user_theme;
+	private String user_sign_date;
+	
 	
 	public String getUser_id() {
 		return user_id;
@@ -14,11 +19,11 @@ public class UsersVO {
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
 	}
-	public String getUser_name() {
-		return user_name;
+	public String getUser_nickname() {
+		return user_nickname;
 	}
-	public void setUser_name(String user_name) {
-		this.user_name = user_name;
+	public void setUser_nickname(String user_nickname) {
+		this.user_nickname = user_nickname;
 	}
 	public String getUser_password() {
 		return user_password;
@@ -26,12 +31,11 @@ public class UsersVO {
 	public void setUser_password(String user_password) {
 		this.user_password = user_password;
 	}
-	
-	public String getUser_email() {
-		return user_email;
+	public String getUser_name() {
+		return user_name;
 	}
-	public void setUser_email(String user_email) {
-		this.user_email = user_email;
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
 	}
 	public String getUser_tel() {
 		return user_tel;
@@ -39,12 +43,38 @@ public class UsersVO {
 	public void setUser_tel(String user_tel) {
 		this.user_tel = user_tel;
 	}
-	public String getUser_nickname() {
-		return user_nickname;
+	public String getUser_birth() {
+		return user_birth;
 	}
-	public void setUser_nickname(String user_nickname) {
-		this.user_nickname = user_nickname;
+	public void setUser_birth(String user_birth) {
+		this.user_birth = user_birth;
 	}
+	public String getUser_class() {
+		return user_class;
+	}
+	public void setUser_class(String user_class) {
+		this.user_class = user_class;
+	}
+	public int getUser_rating() {
+		return user_rating;
+	}
+	public void setUser_rating(int user_rating) {
+		this.user_rating = user_rating;
+	}
+	public int getUser_theme() {
+		return user_theme;
+	}
+	public void setUser_theme(int user_theme) {
+		this.user_theme = user_theme;
+	}
+	public String getUser_sign_date() {
+		return user_sign_date;
+	}
+	public void setUser_sign_date(String user_sign_date) {
+		this.user_sign_date = user_sign_date;
+	}
+	
+	
 	
 	
 }
