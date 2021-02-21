344f5cf66fe51df31bae32e8df75e780d2a23378
Scarl-ett <chdnjs7610@gmail.com>
2021-02-22 08:58:12 +0900

    - 로그인 오류 수정 - 약관동의 페이지 완료 - 회원가입 페이지 완료 - 회원가입 -> 인증코드, 프로필사진 및 팔로우 등록 화면 페이지 css, js 작업중 - 메인화면 html 태그 완료 css, js 작업중

--- playddit/WebContent/script/modal.js
+++ playddit/WebContent/script/modal.js
@@ -6,11 +6,17 @@ $(document).ready(function(){
     
     $(".fas").click(function(){
 		$("#modal").fadeIn(200);
-		$("#modalBody").delay(100).animate({marginTop:0},400);
+		$("#modalBody, #condiModal").delay(100).animate({marginTop:0},400);
 	});
 	
-	$("#close").click(function(){
-		$("#modalBody").animate({marginTop:"300px"},300);
+	$("#close, #modal").click(function(){
+		$("#modalBody, #condiModal").animate({marginTop:"300px"},300);
 		$("#modal").delay(100).fadeOut(300);
+	});
+    
+    // 회원가입
+    $("#joinBtn").click(function(){
+		$("#modal").fadeIn(200);
+        $("#codeModal").delay(100).animate({marginTop:0},400);
 	});
 });
