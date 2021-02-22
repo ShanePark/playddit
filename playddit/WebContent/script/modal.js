20e8947717130515d852f8bb9e1f093a1d5f5f01
Scarl-ett <chdnjs7610@gmail.com>
2021-02-22 10:10:00 +0900

    forgot password 모달창 오류 수정

--- playddit/WebContent/script/modal.js
+++ playddit/WebContent/script/modal.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
 		$("#modalBody, #condiModal").delay(100).animate({marginTop:0},400);
 	});
 	
-	$("#close, #modal").click(function(){
+	$("#close").click(function(){
 		$("#modalBody, #condiModal").animate({marginTop:"300px"},300);
-		$("#modal").delay(100).fadeOut(300);
+		$("#modal").delay(200).fadeOut(300);
 	});
     
     // 회원가입
