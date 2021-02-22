25663011f605559ed9f2df9546191056dccaa750
ShanePark <psh40963@naver.com>
2021-02-22 20:35:42 +0900

    비밀번호 찾기 기능을 index.html에 붙였습니다.
    1. index.html에 쓰일 스크립트들을 index.js 로 만들어서 script 폴더에 넣었습니다.
    2. form 타입 액션이 아닌 , input button 으로 처리하면서, 폼에 있던 action과 method를 삭제했습니다. span id="msg"에는 기본 텍스트 값을 지우고, 나중에 텍스트를 넣도록 하였습니다.
    3. 모달이 닫힐때 $("#modal").fadeOut(200); 를 썼는데, 혹시 따로 원하는 애니메이션이 있으면 말씀해주세요

---
+++ playddit/WebContent/script/index.js
@@ -0,0 +1,54 @@
+function sendTempPass(){
+ 	let email = $('#findmail').val().trim();
+	
+	$.ajax({
+		url : '/playddit/users/idCheck.do',
+		type : 'post',
+		data : { 'id' : email },
+		success : function(res){
+			let result = res.result;
+			if(result=='1'){
+				$('#msg').text('이메일 발송중입니다. 잠시만 기다려주세요.');
+				sendTempPass2(email);
+			}else{
+				$('#msg').text('가입되지 않은 이메일 입니다.');
+			}
+		},
+		error : function(xhr){
+			alert("상태 : " + xhr.status);
+		},
+		dataType : 'json'
+	})
+}
+
+function sendTempPass2(email){
+	$.ajax({
+		url : '/playddit/users/sendTempPass2.do',
+		type : 'post',
+		data : {'email' : email},
+		success : function(res){
+			if(res.result=='1'){
+				alert(email+"로 이메일 전송이 완료되었습니다.");
+				$("#modal").fadeOut(200);
+			}else{
+				alert(res.result);
+			}
+		},
+		error : function(xhr){
+			alert("상태 : " + xhr.status);
+		},
+		dataType : 'json'
+		
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
