a49bd1058e83dd05a4980fd0fd1249486c0827f3
ShanePark <psh40963@naver.com>
2021-02-25 08:47:36 +0900

    main에 로그인 불러오는 세션 정보들을 불러오도록 구현
    후

---
+++ playddit/WebContent/script/main.js
@@ -0,0 +1,22 @@
+function loadProfile(){
+	
+	$.ajax({
+		url : '/playddit/login/login.do',
+		type : 'post',
+		success : function(res){
+			$('#userName').append(res.user_nickname);	
+			$('#userMail').append(res.user_id);	
+			$('#allFeed').append(res.allfeed);	
+			$('#Follower').append(res.follower);	
+			$('#Following').append(res.following);	
+			$('#className').append(res.className);	
+			$('#self').append(res.user_bio);	
+			
+		},
+		error : function(xhr){
+			alert("상태 : " + xhr.status);
+		},
+		dataType : 'json'
+	})
+
+}
