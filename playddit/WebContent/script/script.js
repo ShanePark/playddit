a01899d3f64279f57a0cf38a3d5e963e285590a2
Scarl-ett <chdnjs7610@gmail.com>
2021-02-25 14:22:30 +0900

    전반적으로 다 수정하고 보완했습니다

--- playddit/WebContent/script/script.js
+++ playddit/WebContent/script/script.js
@@ -1,22 +1,36 @@
 $(function(){
     var stat = true;
     
-    $("#ham").click(function(){
+    $("#ham, #mham").click(function(){
         if(stat){
+            $('body').addClass('scrollOff').on('scroll touchmove mousewheel', function(e){
+                e.preventDefault();
+            });
             $("#back").stop().fadeIn(200);
             $("#mnav").css({
                 marginLeft: 0
             });
-            $("#ham").addClass("ex");
+            $("#ham, #mham").addClass("ex");
             stat = false;
         }else{
             $("#mnav").css({
                 marginLeft: "-120%"
             });
             $("#back").stop().fadeOut(200);
-            $("#ham").removeClass("ex");
+            $("#ham, #mham").removeClass("ex");
             stat = true;
+            $('body').removeClass('scrollOff').off('scroll touchmove mousewheel');
         }
+    });
+    
+    $("#back").on("click", function(){
+        $("#mnav").css({
+            marginLeft: "-120%"
+        });
+        $("#back").stop().fadeOut(200);
+        $("#ham, #mham").removeClass("ex");
+        stat = true;
+        $('body').removeClass('scrollOff').off('scroll touchmove mousewheel');
     });
     
     $(window).resize(function(){
@@ -24,8 +38,9 @@ $(function(){
         if(winW >= 1024){
             $("#mnav, #back").removeAttr("style");
         }else{
+            $("#back").fadeOut();
             $("#mnav").css({marginLeft: "-120%"});
-            $("#ham").removeClass("ex");
+            $("#ham, #mham").removeClass("ex");
             stat = true;
         }
     });
