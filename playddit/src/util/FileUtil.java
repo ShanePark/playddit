2162bec609c8331a4c37e6e942bd961b003f3c9a
Shane_Park <kkobuk@hotmail.co.kr>
2021-03-01 12:02:29 +0900

    window 경로도 설정 완료 입니다

--- playddit/src/util/FileUtil.java
+++ playddit/src/util/FileUtil.java
@@ -9,7 +9,7 @@ import java.io.File;
  */
 public class FileUtil {
 	
-	public static final String WIN_PROJECT_PATH = "";
+	public static final String WIN_PROJECT_PATH = "D:\\A_TeachingMaterial\\playddit\\playddit";
 	public static final String MAC_PROJECT_PATH = "/Users/shane/Documents/GitHub/playddit/playddit";
 	public static final String SEP = File.separator;
 	
