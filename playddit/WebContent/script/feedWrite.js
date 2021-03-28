d0106cc2e6d5070b36fe160a69f2f462d05f23ef
ShanePark <psh40963@naver.com>
2021-03-28 10:19:51 +0900

    파일 업로드 기능 구현을 위해 js 파일 분리
    파일 업로드 클릭시에 한번만 이벤트 발생하고 다음부터 아무리 + 버튼을 눌러도 반응하지 않는 문제가 있길래 fileBox 클릭 이벤트를 태그상의 on click 이벤트에서 아이에 script 쪽으로 뺐습니다. 여러번 클릭 가능합니다.

---
+++ playddit/WebContent/script/feedWrite.js
@@ -0,0 +1,109 @@
+$(function(){
+	
+	// 파일 업로드 버튼눌렀을때 input_imgs (파일업로드 input) 실행.css 때문에 우회
+	$('#fileBtn').on('click',function(){
+		$('#input_imgs').click();
+	})
+	
+	/**
+		이미지 업로드시 미리보기 이벤트
+	 */
+	$(document).ready(function() {
+		$("#input_imgs").on("change", handleImgsFilesSelect);
+		
+		$("#fileBox").on("click", ".deleteBtn", function(){
+			$(this).parent(".file").remove();
+		});
+	});
+	
+	/**
+		Keyup 시에 본문 글자수 세주는 이벤트
+	 */	
+	$('#content').on('keyup', function() {
+				$('#bytes').html("("+$(this).val().length+"자 / 최대 700자)");
+
+				if($(this).val().length > 700) {
+					$(this).val($(this).val().substring(0, 700));
+					$('#bytes').html("(700자 / 최대 700자)");
+				}
+			});
+            
+ 
+	/**
+		업로드 버튼 이벤트 
+	 */
+    $("#uploadBtn a span").click(function() {
+        var btn = $(this).parent().parent();
+        var loadSVG = btn.children("a").children(".load");
+        var loadBar = btn.children("div").children("span");
+        var checkSVG = btn.children("a").children(".check");
+        
+        btn.children("a").children("span").fadeOut(200, function() {
+          btn.children("a").animate({
+            width: 56 
+          }, 100, function() {
+            loadSVG.fadeIn(300);
+            btn.animate({
+              width: 320  
+            }, 200, function() {
+              btn.children("div").fadeIn(200, function() {
+                loadBar.animate({
+                  width: "100%" 
+                }, 1000, function() {
+                  loadSVG.fadeOut(200, function() {
+                    checkSVG.fadeIn(200, function() {
+                      setTimeout(function() {
+                        btn.children("div").fadeOut(200, function() {
+                          loadBar.width(0);
+                          checkSVG.fadeOut(200, function() {
+                            btn.children("a").animate({
+                              width: 150  
+                            });
+                            btn.animate({
+                              width: 150
+                            }, 300, function() {
+                              btn.children("a").children("span").fadeIn(200);
+                            });
+                          });
+                        });
+                      }, 1000); 
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+        
+        setTimeout(function() {
+            location.href="feed.jsp";
+		}, 3000);
+        
+    });	 
+	
+	
+	
+});
+
+
+
+function handleImgsFilesSelect(e) {
+	var files = e.target.files;
+	var filesArr = Array.prototype.slice.call(files);
+
+	filesArr.forEach(function(f) {
+		if(!f.type.match("image.*")) {
+			alert("이미지만 업로드 하실 수 있습니다.");
+			return;
+		}
+
+		sel_files.push(f);
+
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			var img_html = '<div class="file"><img src=\"' + e.target.result + '\" /><div class=\"deleteBtn\"><i class=\"far fa-trash-alt\"></i></div></div>';
+			$("#fileBox").append(img_html);
+		}
+		reader.readAsDataURL(f);
+	});
+}
