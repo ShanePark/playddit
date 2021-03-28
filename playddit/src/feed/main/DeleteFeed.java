d60624ea60a5552627e7e63aacb0dd7aefc2ab7a
ShanePark <psh40963@naver.com>
2021-03-29 00:18:55 +0900

    게시물 삭제시 게시물갯수 -1, 게시물 추가시 게시물갯수 +1 되도록 코드 수정

--- playddit/src/feed/main/DeleteFeed.java
+++ playddit/src/feed/main/DeleteFeed.java
@@ -27,6 +27,17 @@ public class DeleteFeed implements IAction {
 			throws ServletException, IOException {	
 		int feedno = Integer.parseInt(request.getParameter("feedno"));
 		
+		// 세션을 받아온다. 
+		HttpSession session = request.getSession();
+		String profileJson = (String) session.getAttribute("profile");
+		Gson gson = new Gson();
+		ProfileVO profile = gson.fromJson(profileJson, ProfileVO.class);
+		
+		// 세션에 작성한 피드 갯수 정보를 -1 해서 새로 저장한다.
+		profile.setAllfeed(profile.getAllfeed()-1);
+		profileJson = new Gson().toJson(profile);
+		session.setAttribute("profile", profileJson);
+		
 		IFeedService service = FeedServiceImpl.getService();
 		service.deleteFeed(feedno);
 		 
