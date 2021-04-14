a1ed8f99ff1009b36c6dabdb8190f8171e66fc9c
ShanePark <psh40963@naver.com>
2021-04-14 19:50:59 +0900

    암호화 테스팅 코드가 테스팅이 충분히 되어서 이제 간결화 시킵니다.

--- playddit/src/util/CryptoTest.java
+++ playddit/src/util/CryptoTest.java
@@ -4,33 +4,14 @@ public class CryptoTest {
 
 	public static void main(String[] args) {
 		try {
-			String id = "psh40963@naver.com";
-			String password = "1234";
-			String key = "playddit"+id+password;	// 16 글자 이상.
-			
-			String result = CryptoUtil.encryptAES256(password, key);
-			String result2 = CryptoUtil.decryptAES256(result, key);
+			String id = "guest@playddit.net";
+			String password = "guest";
 					
 			System.out.println(" 원본 데이터 : " +password);
-			System.out.println(" 암호화 데이터 : " +result);
-			System.out.println(" 복호화 데이터 : " + result2);
 			
-			String shaResult = CryptoUtil.encryptSha512(result);
-			System.out.println("이중 단방향 암호화된 데이터 : " + shaResult);
+			String encPass = CryptoUtil.encryptPass(id, password);
+			System.out.println("단방향암호화 된 데이터 : " + encPass);
 			
-			String shaResult2 = CryptoUtil.encryptPass(id, password);
-			System.out.println("result에서 단방향암호화 된 데이터 : " + shaResult2);
-			
-			String originalPass = "wE09QgX6bbZ5dfKNgOJa2w==";
-			String shaPass = CryptoUtil.encryptSha512(originalPass);
-			System.out.println("Original Pass");
-			System.out.println(originalPass);
-			System.out.println("Sha Pass");
-			System.out.println(shaPass);
-			
-			System.out.println(result.length());
-			System.out.println(shaResult.length());
-			System.out.println(shaPass.length());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
